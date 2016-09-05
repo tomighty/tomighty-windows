eeec893aff87d97bae22ad285e3f683f7e07c74b
Celio Cidral Jr <ccidral@gmail.com>
2016-09-05 20:07:22 -0300

    Change some assembly information.

--- Tomighty.Windows/Properties/AssemblyInfo.cs
+++ Tomighty.Windows/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tomighty.Windows")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Tomighty")]
+[assembly: AssemblyDescription("Tomighty for Windows")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tomighty.Windows")]
