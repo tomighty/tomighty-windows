219e429cbdca681453327e192f33973a74be066a
Celio Cidral Jr <ccidral@gmail.com>
2017-05-20 15:50:28 -0300

    Fix many issues with the build script

--- dist.bat
+++ dist.bat
@@ -1,8 +1,36 @@
 @echo off
 
-REM msbuild Tomighty.sln /t:rebuild /p:Configuration=Release
+set version=%1
 
-for /f %%i in ('git rev-parse --abbrev-ref HEAD') do set version=%%i
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~
+echo %version%
+echo ~~~~~~~~~~~~~~~~~~~~~~~
+echo.
+
+if "%version%"=="" (goto use_latest_tagname_as_version_number) else goto begin
+
+
+
+:use_latest_tagname_as_version_number
+
+for /f %%i in ('git rev-list --tags --max-count=1') do set commit=%%i
+for /f %%i in ('git describe --tags %commit%') do set version=%%i
+
+
+
+:begin
+
+echo ==========================================================================
+echo Building Tomighty %version%
+echo ==========================================================================
+echo.
+
+msbuild Tomighty.sln /t:rebuild /p:Configuration=Release
+
+
+
+:package
 
 set dirname=tomighty-windows-%version%
 set zipfile=dist\%dirname%.zip
@@ -27,12 +55,20 @@ if exist %zipfile% (
 
 mkdir %dest%\Resources
 
-copy LICENSE.txt %dest%
-copy NOTICE.txt %dest%
-copy %src%\Tomighty.Windows.exe %dest%
-copy %src%\Tomighty.Core.dll %dest%
-copy %src%\Newtonsoft.Json.dll %dest%
-copy %src%\Microsoft.Toolkit.Uwp.Notifications.dll %dest%
-xcopy /s %src%\Resources %dest%\Resources
+xcopy /f LICENSE.txt %dest%
+xcopy /f NOTICE.txt %dest%
+xcopy /f %src%\Tomighty.Windows.exe %dest%
+xcopy /f %src%\Tomighty.Core.dll %dest%
+xcopy /f %src%\Newtonsoft.Json.dll %dest%
+xcopy /f %src%\Microsoft.Toolkit.Uwp.Notifications.dll %dest%
+xcopy /f /s %src%\Resources %dest%\Resources
 
 powershell -executionpolicy bypass -file pack.ps1 "%dest%" "%zipfile%"
+
+
+
+echo.
+echo --------------------------------------------------------------------------
+echo Package: %zipfile%
+echo Build finished
+echo --------------------------------------------------------------------------
